e93e44f8052598dc2ecf381ddd21f3f4008c681b
zelebwr <jonathanzelig6013@gmail.com>
2025-06-17 23:36:48 +0700

    feat: add tambahBiasa()

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@
 #include <map>
 using namespace std;
 
+int tambahBiasa(const int a, const int b) {
+    return a + b;
+}
 
 int main(){
+    int hasil = tambahBiasa(5, 10);
+    cout << "Hasil: " << hasil << endl;
     return 0;
 }
